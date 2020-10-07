40cf405d1496edde3a5282e96686c6a3f1029c5a
Ryan Hunter <36390187+systemsvanguard@users.noreply.github.com>
2020-10-07 14:05:33 -0400

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+// src/pages/Home.js
+import React from 'react';
+import Finance03 from '../images/finance03.jpg'
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1 className="is-size-2 has-text-weight-bold has-text-danger  has-text-centered">Home</h1>
+      <img src={Finance03} className="img_responsive" alt="Home" title="Home" /><br />
+      <p>Haul wind line me case shot avast mutiny Nelsons folly. Weigh anchor heave to hardtack grog blossom quarterdeck doubloon sloop. Corsair clipper doubloon sloop loot American Main Shiver me timbers. Reef sails Plate Fleet ye yard walk the plank lugsail sloop.</p><br />
+	  </div>
+  )
+}
+
+export default Home;
